28dbe12026d5dec0ff5e651b3b5939a2a4ddbda0
code99-ash <oyetikh1@gmail.com>
2022-12-31 12:42:39 +0100

    added hello route to test on production

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return "Hello World";
 });
 
 Route::get('/{any}', function() {
